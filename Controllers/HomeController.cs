1100b0f916c07c7c4e631df04b206eb52003abe2
ana004 <paulaana08002019@gmail.com>
2022-08-16 15:27:19 -0300

    adicionado formulario

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,12 @@ using ExemploAspNetMvc.Models;
 
 namespace ExemploAspNetMvc.Controllers;
 
+public class UserCreateDto
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+}
+
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
@@ -31,6 +37,21 @@ public class HomeController : Controller
     public string TesteQueryString([FromQuery] string q, [FromQuery] string nome)
     {
         return $"Chegou aqui {q} e {nome}";
+    }
+
+    // public string TesteFormData([FromForm] string nome, [FromForm] string email) 
+    // {
+    //     return $"Nome: {nome}, E-mail: {email}";
+    // }
+
+    public IActionResult Formulario()
+    {
+        return View();
+    }
+
+    public string TesteFormData([FromForm] UserCreateDto userCreateDto, [FromHeader] string x) 
+    {
+        return $"Nome: {userCreateDto.Nome}, E-mail: {userCreateDto.Email}, x: {x}";
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
